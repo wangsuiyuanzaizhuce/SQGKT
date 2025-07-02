8c6018c7980f38a9e4034e7a2022dda468acc765
Yingying933 <116471618+Yingying933@users.noreply.github.com>
2025-07-02 08:14:01 +0800

    Update train_test.py

--- train_test.py
+++ train_test.py
@@ -35,7 +35,7 @@ params = {
     'prefetch_factor': 4,
     'agg_hops': 3,
     'emb_dim': 100,
-    'hard_recap': False,
+    'hard_recap': True,
     'dropout': (0.2, 0.4),
     'rank_k': 10,
     'k_fold': 5
